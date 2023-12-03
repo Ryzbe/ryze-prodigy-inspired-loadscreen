6553204f6177fdfdfb078e305c5c4073d45ac73d
Ryze Road Shop <101842311+Ryzbe@users.noreply.github.com>
2023-12-02 21:48:01 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,19 +2,18 @@ fx_version 'cerulean'
 author 'Ryze Roadshop <j.doe@example.com>'
 description 'Ryze Roadshop Loading Screen'
 version '1.0.0'
-
 loadscreen_cursor 'yes'
 game 'gta5'
 loadscreen 'html/index.html'
+lua54 'yes'
 
 files {
     'html/index.html',
     'html/logo.png',
-    'html/main.css',
-    'html/main.js',
+    'html/index.css',
+    'html/index.js',
     'html/img/bkg.png'
 }
 
 client_script 'client.lua'
 server_script 'server.lua'
-lua54 'yes'
